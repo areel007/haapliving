9944bc74602fcf72e6235131327908b572393247
Sunday Morenikeji <65197762+areel007@users.noreply.github.com>
2023-05-22 18:09:25 +0100

    fix

--- server.js
+++ server.js
@@ -15,4 +15,4 @@ mongoose
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`App running on port ${process.env.PORT}`));
+app.listen(port, () => console.log(`App running on port ${port}`));
